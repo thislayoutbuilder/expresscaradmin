d6bee11b0fecdc1728e4c044d4098ac96dc21da6
Vic Dev <thislayoutbuilder@gmail.com>
2022-05-11 15:36:59 -0500

    Preparacion de mi server en express js, siempre utilizo el crud de programming languaes, es una buena base para comenzar sobre mysql, usando mysql2 driver

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "sql5.freemysqlhosting.net",
+      user: "sql5491637",
+      port: 3306,
+      password: "v4KWzm6GuX",
+      database: "sql5491637",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
